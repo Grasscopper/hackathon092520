3aa9c79cf5e5f77f752aaf38a9843af339eab011
Grasscopper <jssstransky@gmail.com>
2020-09-25 17:12:34 -0400

    Nothing to do with categories. Accounts index page using local storage.

---
+++ src/AccountTile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AccountTile = (props) => {
+  return (
+    <li>{props.account.title}</li>
+  )
+}
+
+export default AccountTile
--- src/Expenses.js
+++ src/Expenses.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import AccountTile from './AccountTile'
 
 const Expenses = (props) => {
+  let [accounts, setAccounts] = useState([])
   let [account, setAccount] = useState({
     title: ""
   })
+
+  useEffect(() => {
+    let storage = window.localStorage
+    let storedAccounts = storage.getItem("accounts") //JSON string
+    if (storedAccounts != null) {
+      setAccounts(JSON.parse(storedAccounts))
+    }
+  }, [])
 
   const update = (event) => {
     event.preventDefault()
@@ -13,21 +24,46 @@ const Expenses = (props) => {
     })
   }
 
-  const postExpense = (event) => {
+  const postAccount = (event) => {
     event.preventDefault()
-    let store = window.localStorage
-    let storeAccount = JSON.stringify(account)
-    store.setItem(storeAccount, storeAccount)
+    let storage = window.localStorage
+    let storedAccounts = storage.getItem("accounts") //JSON string
+    if (storedAccounts != null) {
+      let accountsArray = JSON.parse(storedAccounts) //JSON to array
+      accountsArray.push(account) //push new account on array
+      let accountsString = JSON.stringify(accountsArray) //Array to JSON
+      storage.setItem("accounts", accountsString) //save new account
+    }
+    else {
+      let accountsArray = []
+      accountsArray.push(account)
+      let accountsString = JSON.stringify(accountsArray)
+      storage.setItem("accounts", accountsString)
+    }
+    setAccounts([
+      ...accounts,
+      account
+    ])
+    clearAccountForm()
+  }
+
+  const clearAccountForm = () => {
     setAccount({
       title: ""
     })
   }
 
+  let accountTiles = accounts.map((account) => {
+    return (
+      <AccountTile account={account} />
+    )
+  })
+
   return (
     <div className="grid-container">
-      <h1>Expenses</h1>
-      <form onSubmit={postExpense} autoComplete="off">
-        <label htmlFor="title">Title </label>
+      <h1>Expense Tracker</h1>
+      <form onSubmit={postAccount} autoComplete="off">
+        <label htmlFor="title">Title</label>
         <input
           id="title"
           name="title"
@@ -36,6 +72,10 @@ const Expenses = (props) => {
           onChange={update} />
         <button type="submit">Save</button>
       </form>
+      <h3>Accounts</h3>
+      <ul>
+      {accountTiles}
+      </ul>
     </div>
   )
 }
